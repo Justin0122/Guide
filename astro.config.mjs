ef55961933131bbb4eedca0da61c16232e0aff1e
Justin Jongstra <00300061@rijnijssel.nl>
2024-06-13 09:05:44 +0200

    added notes / links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Guide',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/justin0122/guide',
 			},
 			sidebar: [
 				{
